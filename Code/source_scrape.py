205566f548815b94e10ec5487515c114d6c73b7d
lucas <lucasspellman98@gmail.com>
2023-01-17 20:44:50 -0600

    add today in logging

--- Code/source_scrape.py
+++ Code/source_scrape.py
@@ -1,4 +1,5 @@
 import argparse
+import datetime
 import logging
 import requests
 
@@ -10,7 +11,7 @@ def parse_args():
 
     parser = argparse.ArgumentParser()
     # FIXME: switch this argument to an optional 
-    parser.add_argument('-o', '--out-path', type = str, default='out.json', help = 'Path to save source info to')
+    parser.add_argument('-o', '--out-path', type=str, default='out.json', help='Path to save source info to')
     args = parser.parse_args()
 
     return args
@@ -19,6 +20,7 @@ def parse_args():
 def main():
     """"Main Function"""
     # Setup logger
+    now = datetime.datetime.now().date()
 
     formatstr = '%(asctime)s: %(levelname)s: %(funcName)s Line: %(lineno)d %(message)s'
     datestr = '%m/%d/%Y %H:%M:%S'
@@ -35,16 +37,16 @@ def main():
     args = parse_args()
 
     # scrape NFL information
-    logging.info('Scraping Source Information')
+    logging.info(f'Scraping Source Information for {now}')
     # TODO: generalize the data request
     # Strategy pattern? Think about what needs to be returned from a data request
     source = requests.get("https://www.bovada.lv/services/sports/event/v2/events/A/description/football/nfl").json()
     source_data = source[0]
 
     # save source info
-    logging.info("Saving Source Information")
+    logging.info(f"Saving Source Information")
     write_json(source_data, args.out_path)
-    logging.info(" ")
+    logging.info("Download Finished")
 
     return
 
